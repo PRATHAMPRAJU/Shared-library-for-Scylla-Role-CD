8a1d914a83511671aec28739c11ed6a64a598f6c
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2025-01-04 17:37:23 +0530

    Update deployRole.groovy

--- vars/deployRole.groovy
+++ vars/deployRole.groovy
@@ -1,5 +1,3 @@
-import org.ansible.Deploy
-
 def call(String inventoryPath, String playbookPath) {
-    Deploy.deploy(inventoryPath, playbookPath, this)
+    org.ansible.Deploy.deploy(inventoryPath, playbookPath, this)
 }
