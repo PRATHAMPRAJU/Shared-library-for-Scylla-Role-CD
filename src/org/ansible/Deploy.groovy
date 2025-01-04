a6d98b5a821a97adaf548dab7f2dc2bbf39f5c52
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2025-01-04 17:35:57 +0530

    Update Deploy.groovy

--- src/org/ansible/Deploy.groovy
+++ src/org/ansible/Deploy.groovy
@@ -2,15 +2,18 @@ package org.ansible
 
 class Deploy {
     static void deploy(String inventoryPath, String playbookPath, def steps) {
-        def result = steps.sh(script: """
-            ansible-playbook -i ${inventoryPath} ${playbookPath}
-        """, returnStatus: true)
+        // Change to the cloned repository directory
+        steps.dir('scylla-Role') {
+            def result = steps.sh(script: """
+                ansible-playbook -i ${inventoryPath} ${playbookPath}
+            """, returnStatus: true)
 
-        if (result != 0) {
-            steps.echo "Deployment failed with exit code: ${result}"
-            steps.error("Deployment failed. Check the logs for details.")
-        } else {
-            steps.echo "Deployment completed successfully."
+            if (result != 0) {
+                steps.echo "Deployment failed with exit code: ${result}"
+                steps.error("Deployment failed. Check the logs for details.")
+            } else {
+                steps.echo "Deployment completed successfully."
+            }
         }
     }
 }
